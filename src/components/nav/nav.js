ac02dab541addbee5cb461afc1209467e427a68f
Francisco Arleo <franciscoarleo336@gmail.com>
2023-12-13 16:47:44 -0500

    testing switch on nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./nav.scss";
-import { BurgerMenu, Button, ProfilePicture } from "../";
+import { BurgerMenu, Button, ProfilePicture, Switch } from "../";
 import { NAV_QUERY, fetchData } from "../../constants";
 
 export const Nav = () => {
@@ -15,11 +15,7 @@ export const Nav = () => {
         <nav>
             <ProfilePicture />
             <div className="options">
-                <Button type="btn btn-icon-primary btn-icon-primary-outline">
-                    <div>
-                        <i className="icon icon-translate"></i>
-                    </div>
-                </Button>
+                <Switch />
                 <BurgerMenu navModels={navModels} />
             </div>
         </nav>
