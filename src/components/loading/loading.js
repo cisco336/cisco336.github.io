434f35f293cc68a5c36fe2076669aaeb7ae2d974
Francisco Arleo <franciscoarleo336@gmail.com>
2023-12-13 23:40:12 -0500

    loading

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./loading.scss";
 
 export const Loading = () => {
-    return <>Loading...</>
+    return (
+        <div className="loading__wrapper">
+            <div className="loading__container">
+                {[1, 2, 3, 4].map((e) => (
+                    <span className="loading__dot"></span>
+                ))}
+            </div>
+        </div>
+    );
 }
